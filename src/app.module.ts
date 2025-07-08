e165e737c8b5b186308677fa5138c46a2e7b4b64
luckbys <lucas.hborges42@gmail.com>
2025-07-07 23:42:46 -0300

    Adiciona suporte à senha do Redis na configuração do BullModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,6 +15,7 @@ import { BullModule } from '@nestjs/bull';
         redis: {
           host: configService.get('REDIS_HOST'),
           port: +configService.get('REDIS_PORT'),
+          password: configService.get('REDIS_PASSWORD'),
         },
       }),
       inject: [ConfigService],
